ad183a93bab15e41129c461f4f91fc846dde2ec0
José Luis Almendarez González <almendarez1002@gmail.com>
2025-10-26 15:09:55 -0600

    First Commit

---
+++ lib.py
@@ -0,0 +1,17 @@
+from torchvision.datasets import MNIST
+from torchvision import transforms
+from PIL import Image
+import os, random
+from torch.utils.data import ConcatDataset
+import matplotlib.pyplot as plt
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+import lib
+import torchvision.models as models
+import torch.nn as nn
+
+### SECCIÓN UNO
+
+### SECCIÓN DOS
+
+### SECCIÓN TRES
